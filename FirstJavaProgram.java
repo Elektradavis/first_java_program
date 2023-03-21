cacd08c03c0c6e0c13d26135b77c6ad2cb9e3305
Peter Mullins <jc310xc@gmail.com>
2023-03-20 20:59:06 -0400

    did some stuff

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -3,5 +3,6 @@ public class FirstJavaProgram {
         System.out.println("My name is Elektra Davis");
         System.out.println("I am 25 years old");
         System.out.println("My hometown is Harmony, PA");
+        System.out.println("Also, Pete says hi :)");
     }
 }

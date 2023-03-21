597aa1cb5e2380adcdb6277a3ab262df6a2e881f
Elektra Davis <elektradavis117@gmail.com>
2023-03-21 11:38:59 -0400

    change hometown

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -2,7 +2,7 @@ public class FirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("My name is Elektra Davis");
         System.out.println("I am 25 years old");
-        System.out.println("My hometown is Harmony, PA");
+        System.out.println("My hometown is Carnegie, PA");
         System.out.println("Also, Pete says hi :)");
     }
 }
